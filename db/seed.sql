7fbfde52060e1c7649c62100b8a3f3a938b2cc9d
Daniel-0117 <nevaslon0117@gmail.com>
2023-06-22 15:44:57 -0700

    sql and server

---
+++ db/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES ("Legal"),
+       ("Finance"),
+       ("Cleaning"),
+       ("Medical");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Lead", 120000, 1),
+       ("Lawyer", 100000, 1),
+       ("Head Accountant", 95000, 2),
+       ("Accountant", 80000, 2),
+       ("Janitorial Lead", 65000, 3),
+       ("Janitor", 45000, 3),
+       ("Medial Lead", 500000, 4),
+       ("Doctor", 250000, 4),
+       ("Nurse", 120000, 4);
+
